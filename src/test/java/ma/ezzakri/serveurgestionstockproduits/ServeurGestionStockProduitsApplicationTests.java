4dd99490b0fc7b4964af4c46659f9c7f391db939
hamza ezzakri <hamzaezzakri98@gmail.com>
2024-10-07 20:05:41 +0100

    first commit

---
+++ src/test/java/ma/ezzakri/serveurgestionstockproduits/ServeurGestionStockProduitsApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.ezzakri.serveurgestionstockproduits;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServeurGestionStockProduitsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
